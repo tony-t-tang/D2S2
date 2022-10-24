3f137c3e2813e877a8a45c0841ce63e279bd4368
dondangp <105334355+dondangp@users.noreply.github.com>
2022-10-23 21:07:12 -0500

    centered PSDoodle

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import '../Assets/Styles/Searchbar.css';
-const Searchbar = () => {
+
+ const Searchbar = () => {
  const [searchInput, setSearchInput] = useState("");
 
  const Icons = [
